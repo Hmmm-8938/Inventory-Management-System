eb1f3aba1b6a3493a1c8583d4e80bb439f8c80f1
Hmmm8938 <39926063+Hmmm-8938@users.noreply.github.com>
2025-02-06 10:20:50 -0700

    QR and Barcode Scanning Functionality
    Currently QR and Barcodes will scan to the app logs. But data is correct

--- Inventory Management System/ContentView.swift
+++ Inventory Management System/ContentView.swift
@@ -7,50 +7,38 @@
 
 import SwiftUI
 import SwiftData
+import CodeScanner
 
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
 
-    var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
+    var body: some View
+    {
+        VStack(spacing: 20)
+        {
+            CodeScannerView(codeTypes: [.code128, .qr], simulatedData: "Paul Hudson") { response in
+                switch response
+                {
+                    case .success(let result):
+                    print("Found code: \(result.string)")
+                    case .failure(let error):
+                    print(error.localizedDescription)
                 }
             }
-        } detail: {
-            Text("Select an item")
-        }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
+            Button("Sign in")
+            {
+                // Handle sign in
             }
+            .padding()
+            .background(RoundedRectangle(cornerRadius: 8).stroke(Color.blue, lineWidth: 2))
+            
+            Button("Sign out")
+            {
+                // Handle sign out
+            }
+            .padding()
+            .background(RoundedRectangle(cornerRadius: 8).stroke(Color.red, lineWidth: 2))
         }
     }
 }
